9ad45ec36bf2e142111b37fa717e0e7a007a3559
Murshud44 <130497183+Murshud44@users.noreply.github.com>
2023-06-08 12:55:02 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber);
+
+let password=''
+function write(){
+let body=document.getElementById('btn');
+let input=document.getElementById('click');
+
+for(let i=0;i<body.value;i++){
+ 
+}
+console.log(write);
+}
+
+
+function generateRandomArray(length, min, max) {
+    const randomArray = [];
+}
